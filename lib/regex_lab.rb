ab1307d1c2e6aabe25940f007f72b1052a3a8528
Ruth <rwangare124@gmail.com>
2023-05-21 15:52:04 +0300

    Completed assignment

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
+    word.match?(/\A[aeiou]/i)
+  end
 
-end
+  def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\bun\w*ing\b/)
+  end
 
-def words_starting_with_un_and_ending_with_ing(text)
+  def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/)
+  end
 
-end
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match?(/^[A-Z].*\W$/)
+  end
 
-def words_five_letters_long(text)
+  def valid_phone_number?(phone)
+    phone.match?(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/)
+  end
 
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
